f6b01970effcf8c6e7d8b447c154ab0d6413a17d
SwenBreitung <Veroxa@web.de>
2024-05-14 20:49:50 +0200

    new fetch like

--- src/app/service/backend.service.ts
+++ src/app/service/backend.service.ts
@@ -20,16 +20,16 @@ export class BackendService {
     fetch(this.videosContentURL)
       .then(response => response.json())
       .then(data => {
-        console.log('allContentbevore',data);
+        console.log('allContentbevore', data);
         this.allContent = data;
-        console.log('allContent',this.allContent); 
+        console.log('allContent', this.allContent);
         // Hier kannst du weiteren Code hinzufügen, um die Daten zu verwenden
       })
       .catch(error => console.error('Error fetching video content:', error));
   }
 
-  getVideoUrl(id:string){
-  return  this.videosContentURL + id;
+  getVideoUrl(id: string) {
+    return this.videosContentURL + id;
   }
 
 
@@ -42,7 +42,7 @@ export class BackendService {
         return response.json();
       })
       .then(data => {
-        console.log('fetchData',data);
+        console.log('fetchData', data);
         return data;
       })
       .catch(error => {
@@ -52,33 +52,34 @@ export class BackendService {
   }
 
   // JSON.stringify({ likeType: likeType })
-  addLike(videoId:string, likeType:string) {
-    fetch(`${this.videosContentURL + videoId}/like`, {
+  addLike(videoId: string, likeType: string) {
+    // fetch(`${this.videosContentURL + videoId}/like/`, {
+      fetch(`http://127.0.0.1:8000/video_content/${videoId}/like/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        //  'Authorization': 'Bearer ' + yourAuthToken ,// Auth-Token des Benutzers
-        body: JSON.stringify({
-          likeType: 'up'  // Annahme, dass 'up' oder 'down' gesendet wird
-      }) // Senden des Like-Typs im Request Body
-      }
+        // 'Authorization': 'Bearer ' + yourAuthToken ,// Auth-Token des Benutzers  
+      },
+      body: JSON.stringify({
+        likeType: 'up'
+      })
     })
-    .then(response => {
-      if (response.ok) {
-        console.log('send data')
-        return response.json();
-      } else {
-        throw new Error('Something went wrong');
-      }
-    })
-    .then(data => {
-      console.log('Like added successfully:', data);
-    })
-    .catch(error => console.error('Error adding like:', error));
+      .then(response => {
+        if (response.ok) {
+          console.log('send data')
+          return response.json();
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .then(data => {
+        console.log('Like added successfully:', data);
+      })
+      .catch(error => console.error('Error adding like:', error));
   }
 
 
-  removeLike(videoId:string) {
+  removeLike(videoId: string) {
     fetch(`/api/videos/${videoId}/like`, {
       method: 'DELETE',
       headers: {
@@ -86,17 +87,17 @@ export class BackendService {
         // 'Authorization': 'Bearer ' + yourAuthToken // Auth-Token des Benutzers
       }
     })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Something went wrong');
-      }
-    })
-    .then(data => {
-      console.log('Like removed successfully:', data);
-      // Update UI entsprechend
-    })
-    .catch(error => console.error('Error removing like:', error));
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong');
+        }
+      })
+      .then(data => {
+        console.log('Like removed successfully:', data);
+        // Update UI entsprechend
+      })
+      .catch(error => console.error('Error removing like:', error));
   }
 }
